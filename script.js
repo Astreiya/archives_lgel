65514d66237df727a1bf9838ef001bb7e8387c60
Astreiya <75164088+Astreiya@users.noreply.github.com>
2023-04-10 20:44:09 +0300

    updated files

---
+++ script.js
@@ -0,0 +1,10 @@
+function tableau(nom,date,camp,comm,num) {
+    lien = "./" + num + "/index.html"; 
+    <tr>
+        <td><a href='lien' target="_blank"><b> nom </b></a></td>
+        <td><span class="day">date</span><span> || </span><span class="time">18h25</span></td>
+        <td class="text_center"><b class="village">camp</b></td>
+        <td><i>comm</i></td>
+    </tr>
+    
+}
